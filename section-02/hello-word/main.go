792a1ce326bb5dbee3f2de2020b9bceeea5c7b30
Eduardo Alexandre <edubrdev@gmail.com>
2024-03-12 14:25:59 -0300

    feat: add Go CLI commands description

---
+++ section-02/hello-word/main.go
@@ -0,0 +1,24 @@
+package main
+
+// https://pkg.go.dev/fmt -> Pacote fmt
+// Descrição -> Package fmt implements formatted I/O with functions analogous to C's printf and scanf. The format 'verbs' are derived from C's but are simpler.
+import "fmt"
+
+func main() {
+	fmt.Println("Hi there!")
+}
+
+// Go CLI
+
+// go build -> apenas compila o programa
+// Ex: ao rodar go build main.go foi gerado o arquivo executável main. Para utilizá-lo, basta rodar ./main
+
+// go run -> compila e executa imediatamente o programa
+
+// go fmt -> formata o código
+
+// go install -> compila e instala um pacote
+
+// go get -> baixa o código fonte do pacote de outra pessoa
+
+// go test -> roda qualquer teste associado ao projeto
